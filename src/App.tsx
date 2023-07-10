e8a06a48394866f29809804d3142f20bc886875d
Alexander Sosov <nookat.develop@gmail.com>
2023-07-10 20:43:01 +0300

    Типизация стейта

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import TodoItem from "./components/TodoItem";
 
+type TypeTodo = {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<string[]>([]);
+  const [] = useState<TypeTodo | null>(null);
+
   return (
     <div className="App">
       <TodoItem id='hello' title='Custom title' completed={false} styles={{
